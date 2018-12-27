9b8da306b12ca5a91d2379983c0f82e7686ec8fb
Victor Mello <victorsmello93@gmail.com>
2018-12-27 17:03:43 -0200

    Merge manual para deploy no gitlab.

--- Exercicios/Arrays/Map2.js
+++ Exercicios/Arrays/Map2.js
@@ -8,9 +8,32 @@ const carrinho = [
 ]
 
 //Desafio: Implementar a função map para retornar um array apenas com os preços 
-const carrinhoObjetos = carrinho.map(function(e){
-    let obj = JSON.parse(e);
-    return obj.Preco;
-})
 
-console.log(carrinhoObjetos);
+//Solução 1: Usando apenas uma chamada do map()
+const carrinhoObjetos = carrinho.map(function(e){
+    // let obj = JSON.parse(e)
+    // return `R$ ${obj.Preco.toFixed(2).replace('.', ',')}`
+    return `R$ ${JSON.parse(e).Preco.toFixed(2).replace('.', ',')}`
+})
+console.log(carrinhoObjetos)
+//------------------------------------------------------------------------------------------------------------------------
+//Solução 2: Usando mais de uma chamada do map()
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.Preco
+const paraValorMonetario = vm => `R$ ${vm.toFixed(2).replace('.', ',')}`
+const carrinhoObjetos2 = carrinho.map(paraObjeto).map(apenasPreco).map(paraValorMonetario)
+console.log(carrinhoObjetos2);
+//------------------------------------------------------------------------------------------------------------------------
+
+//Solução 3: Usando a forma tradicional
+const apenasPreco2 = (c) => {
+    const carrinhoObjetos2 = [];
+    for(var i = 0; i < c.length; i++){
+        // let obj2 = JSON.parse(c[i])
+        // carrinhoObjetos2[i] = `R$ ${obj2.Preco.toFixed(2).replace('.', ',')}`
+        carrinhoObjetos2[i] = `R$ ${JSON.parse(c[i]).Preco.toFixed(2).replace('.', ',')}`
+    }
+    return carrinhoObjetos2;
+} 
+
+console.log(apenasPreco2(carrinho))
