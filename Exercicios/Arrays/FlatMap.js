27b15fef6150894ed25bb5541f29b7992a493fa1
Victor Mello <victorsmello93@gmail.com>
2018-12-29 19:58:00 -0200

    Praticando a função flatMap.

--- Exercicios/Arrays/FlatMap.js
+++ Exercicios/Arrays/FlatMap.js
@@ -34,14 +34,20 @@ const getNotaAluno = aluno => aluno.nota;
 const getNotasDaTurma = turma => turma.alunos.map(getNotaAluno);
 
 
-//Obtendo um array com as notas de todos os alunos
+/*Obtendo um array com as notas de todos os alunos, sendo que no resultado é retornado
+um array contendo todos os arrays de alunos*/
 const notas1 = escola.map(getNotasDaTurma);
 console.log(notas1);
-
+//--------------------------------------------------------------------------------------------------------------------------
 //Obtendo as notas dos alunos de todas as turmas usando uma única estrutura de array
+console.log([].concat([ 8.1, 5.5, 7, 9 ],[ 8.9, 6.5, 7, 8.5 ]))
+//É equivalente ao trecho abaixo:
+console.log(escola.concat.apply([], escola.map(getNotasDaTurma)))
+
 Array.prototype.flatMap = function(callback) { 
     return Array.prototype.concat.apply([], this.map(callback));
 }
 
 const notas2 = escola.flatMap(getNotasDaTurma);
 console.log(notas2);
+//--------------------------------------------------------------------------------------------------------
