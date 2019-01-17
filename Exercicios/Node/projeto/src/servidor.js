2e237be32a1b0243a389df34cea3c9567f6ff8eb
Victor Mello <victorsmello93@gmail.com>
2019-01-17 11:25:21 -0200

    Finalização da implementação do CRUD básico de cadastro de produtos com API REST

--- Exercicios/Node/projeto/src/servidor.js
+++ Exercicios/Node/projeto/src/servidor.js
@@ -5,6 +5,9 @@ const porta = 3003
 const express = require('express')
 const app = express()
 const bancoDados = require('./bancoDados')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 // app.get('/produtos', (req, res, next) => {
 //  console.log('Middleware 1')
@@ -26,9 +29,25 @@ app.get('/produtos/:id', (req, res, next) => {
 
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto)
+})
+
+//Atualizando o registro de produto
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco,
+        id: req.params.id
+    })
+    res.send(produto)
+})
+
+//Excluindo o registro de produto
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDados.excluirProduto(req.params.id)
     res.send(produto)
 })
 
