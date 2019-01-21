e34074b0ddd38df26a4e2095812bc3e0de34293c
Victor Mello <victorsmello93@gmail.com>
2019-01-21 19:12:12 -0200

    Entrada e saída padrão JavaScript

---
+++ Exercicios/Node/entradaESaida.js
@@ -0,0 +1,21 @@
+const anonimo = process.argv.indexOf('-a') !== -1
+// console.log(anonimo)
+
+//A tela irá apresentar a mensagem no console toda vez que o usuário executa o flag -a
+if(anonimo){ 
+    process.stdout.write('Olá mundo!\n')
+    process.exit() //Matando o processo
+} else {
+    process.stdout.write('Informe o seu nome: ') 
+    process.stdin.on('data', data => { 
+        const nome = data.toString().replace('\n', '')
+
+        process.stdout.write(`Olá, ${nome}!!\n`)
+        process.exit()
+    })
+}
+
+//process.stdout.write() - Saída de dados no console
+//process.stdin.on('nomeEvento') - Entrada de dados no console
+
+//Por padrão, o evento mais utilizado é ondata, onde o mesmo é disparado quando o usuário pressiona ENTER
