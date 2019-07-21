e2afdbf72a309d6d1dc14b093e105648bee0fa9b
Victor Mello <victorsmello93@gmail.com>
2019-07-21 13:18:31 -0300

    Capítulo de formulários (parte 3)

--- ExerciciosHTML/exercicios/form/server.js
+++ ExerciciosHTML/exercicios/form/server.js
@@ -7,11 +7,33 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}))
 
 // Realizando requisição para url /usuarios
+// app.get('/usuarios', (req, resp) => {
+//     // Obtendo os dados preenchidos do formulário no corpo da requisição
+//     console.log(req.body);
+
+//     // Usando o GET, para que os dados sejam retornados para o backend, utiliza-se query string, e não no corpo da requisição
+//     console.log(req.query);
+     
+//     resp.send('<h1>Usuário incluído com sucesso!</h1>');
+// });
+
+// Realizando requisição para url /usuarios
 app.post('/usuarios', (req, resp) => {
+    // Obtendo os dados preenchidos do formulário no corpo da requisição
+    console.log(req.body);
+     
+    resp.send('<h1>Usuário incluído com sucesso!</h1>');
+});
+
+
+app.post('/usuarios/:id', (req, resp) => {
+    // Obtendo o parâmetro id na requisição
+    console.log(req.params.id);
+
     // Obtendo os dados preenchidos do formulário no corpo da requisição
     console.log(req.body); 
 
-    resp.send('<h1>Dados gravados com sucesso!</h1>');
+    resp.send('<h1>Usuário alterado com sucesso!</h1>');
 });
 
 // Escutando a porta 3003 para aguardar a requisição de dados do formulário
