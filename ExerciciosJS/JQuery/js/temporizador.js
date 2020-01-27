dcc48d3a7dedd8c4fadf3252404bb6f6801822c7
Victor Mello <victorsmello93@gmail.com>
2020-01-27 13:29:27 -0300

    Refatoração do plugin do contador: Foi criada 2 constantes para criar um span: uma para representar os dígitos, e a outra para representar o separador de hora e minuto

--- ExerciciosJS/JQuery/js/temporizador.js
+++ ExerciciosJS/JQuery/js/temporizador.js
@@ -4,44 +4,34 @@
         /*Mesclando o que tiver de valor padrão com o valor passado
           para a chamada da função, através da função
           extend()*/
-        const opcoesFinais = $.extend({
+    const opcoesFinais = $.extend({
             /*Mensagem padrão caso não seja passada a 
             mensagem de fora*/
             mensagem: 'Em breve!',
             horario: '23:59:59'
         }, opcoes)
     
-    // const hora = {
-    //     horaDezena: '0',
-    //     horaUnidade: '0',
-    //     separadorHora: ':',
-    //     minutoDezena: '0',
-    //     minutoUnidade: '0',
-    //     separadorMinuto: ':',
-    //     segundoDezena: '0',
-    //     segundoUnidade: '0'
-    // }    
+    const digito = '<span class="digito">'
+    const separador = '<span class="separador">'
 
-    const conteudo = `
-    <span class="digito">0</span>
-    <span class="digito">0</span>
-    <span class="separador">:</span>
-    <span class="digito">0</span>
-    <span class="digito">0</span>
-    <span class="separador">:</span>
-    <span class="digito">0</span>
-    <span class="digito">0</span>    
-    `
+    const conteudo = {
+        horaDezena: $(digito).html('0'),
+        horaUnidade: $(digito).html('0'),
+        minutoDezena: $(digito).html('0'),
+        minutoUnidade: $(digito).html('0'),
+        segundoDezena: $(digito).html('0'),
+        segundoUnidade: $(digito).html('0'),
+        separadorHora: $(separador).html(':'),
+        separadorMinuto: $(separador).html(':')
+    }
+
     // const horaDezena = $('<span class="digito">').html('0')
     // const horaUnidade = $('<span class="digito">').html('0')
     // const minutoDezena = $('<span class="digito">').html('0')
     // const minutoUnidade = $('<span class="digito">').html('0')
     // const segundoDezena = $('<span class="digito">').html('0')
     // const segundoUnidade = $('<span class="digito">').html('0')
-
-    // const separadorHora = $('<span class="separador">').html(':')
-    // const separadorMinuto = $('<span class="separador">').html(':')
-              
+          
     const mensagem = $('<div class="mensagem">').html(opcoesFinais.mensagem)
     
     $(this).addClass('temporizador')
@@ -49,10 +39,11 @@
     // $(this).append(horaDezena, horaUnidade, separadorHora, 
     //                 minutoDezena, minutoUnidade, separadorMinuto,
     //                 segundoDezena, segundoUnidade, mensagem)
+        
+    $(this).append(conteudo.horaDezena, conteudo.horaUnidade, conteudo.separadorHora,
+                    conteudo.minutoDezena, conteudo.minutoUnidade, conteudo.separadorMinuto,
+                    conteudo.segundoDezena, conteudo.segundoUnidade, mensagem) 
     
-    $(this).append(conteudo.trim(), mensagem)
-
-    const html = conteudo.match(/<span.+>\d<\/span>/g)
     const regex = new RegExp(/(\d\d):(\d\d):(\d\d)/)    
     const horarioAlvo = regex.exec(opcoesFinais.horario)    
 
@@ -67,20 +58,20 @@
 
         if(diferencaEmMili >= 0){
             const diferenca = regex.exec(new Date(diferencaEmMili).toISOString())
-
-            $(html[0]).html(diferenca[1][0])
-            $(html[1]).html(diferenca[1][1])
-            $(html[2]).html(diferenca[2][0])
-            $(html[3]).html(diferenca[2][1])
-            $(html[4]).html(diferenca[3][0])
-            $(html[5]).html(diferenca[3][1])
-
+            
             // horaDezena.html(diferenca[1][0])
             // horaUnidade.html(diferenca[1][1])
             // minutoDezena.html(diferenca[2][0])
             // minutoUnidade.html(diferenca[2][1])
             // segundoDezena.html(diferenca[3][0])
             // segundoUnidade.html(diferenca[3][1])
+
+            conteudo.horaDezena.html(diferenca[1][0])
+            conteudo.horaUnidade.html(diferenca[1][1])
+            conteudo.minutoDezena.html(diferenca[2][0])
+            conteudo.minutoUnidade.html(diferenca[2][1])
+            conteudo.segundoDezena.html(diferenca[3][0])
+            conteudo.segundoUnidade.html(diferenca[3][1])
         }
         else{
             clearInterval(temporizador)
